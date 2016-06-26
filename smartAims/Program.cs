ee9f73374e886cbdf4745c93b15f5d44a53aada3
Nrangu <formywork2009@yandex.ru>
2016-06-26 23:12:24 +0300

    start project SMARTAims

---
+++ smartAims/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using smartAimsDomain;
+namespace smartAims
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Domains domain = new Domains();
+            domain.GetData();
+            Console.ReadKey();
+        }
+    }
+}
