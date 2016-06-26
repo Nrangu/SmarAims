b29cf12a7ae1280a712558a86c7d3a3ae70eab32
Nrangu <formywork2009@yandex.ru>
2016-06-26 23:14:32 +0300

    start

---
+++ smartAimsDomain/SmartAimsDomain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smartAimsDomain
+{
+    public class Domains
+    {
+        public void GetData()
+        {
+            Console.WriteLine("Smart aims Domains");
+        }
+    }
+}
