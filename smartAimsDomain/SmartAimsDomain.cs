f646dda9d1adf6c2fbc653ee609e013d43e734be
Nrangu <formywork2009@yandex.ru>
2016-06-28 00:25:17 +0300

    28.06.2016

--- smartAimsDomain/SmartAimsDomain.cs
+++ smartAimsDomain/SmartAimsDomain.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data.Common;
+using System.Data;
+using System.IO;
 
 namespace smartAimsDomain
 {
@@ -11,6 +15,10 @@ namespace smartAimsDomain
         public void GetData()
         {
             Console.WriteLine("Smart aims Domains");
+            string databaseName = @"cyber.db";
+            SQLiteConnection. CreateFile(databaseName);
+            Console.WriteLine(File.Exists(databaseName) ? "База данных создана" : "Возникла ошиюка при создании базы данных");
+            Console.ReadKey(true);
         }
     }
 }
