5c52aacb78458a98ff27f5fdd695ca120833b07c
Nrangu <formywork2009@yandex.ru>
2016-07-07 22:43:21 +0300

    07/07/16

--- smartAimsDomain/SmartAimsDomain.cs
+++ smartAimsDomain/SmartAimsDomain.cs
@@ -10,15 +10,48 @@ using System.IO;
 
 namespace smartAimsDomain
 {
-    public class Domains
+    public class SQLiteDomain
     {
-        public void GetData()
+        private SQLiteConnection connection = null;
+        private string DBname = null;
+
+        public bool isConnected {
+            get{  return connection !=null?  true: false; }    
+        }
+
+        public bool connect(string DBname)
         {
-            Console.WriteLine("Smart aims Domains");
-            string databaseName = @"cyber.db";
-            SQLiteConnection. CreateFile(databaseName);
-            Console.WriteLine(File.Exists(databaseName) ? "База данных создана" : "Возникла ошиюка при создании базы данных");
-            Console.ReadKey(true);
+            this.DBname = DBname;
+
+            connection = new SQLiteConnection(string.Format("Data Source={0};", this.DBname));
+            connection.Open();
+            return true;
+        }
+        public void GetAims()
+        {
+            //Console.WriteLine("Smart aims Domains");
+            //string databaseName = @"DBaims.db";
+            //SQLiteConnection connection = new SQLiteConnection(string.Format("Data Source={0};", databaseName));
+            //connection.Open();
+            SQLiteCommand command = new SQLiteCommand("SELECT * FROM 'aims';", connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            foreach (DbDataRecord record in reader)
+            {
+                Console.WriteLine(string.Format("{0} {1} {2} ", record["id"], record["title"], record["description"]));
+            }
+        }
+    }
+    public class Controller
+    {
+        private SQLiteDomain domain = new SQLiteDomain();
+        public  void GetAims()
+        {
+            if ( !domain.isConnected)
+            {
+                domain.connect(@"DBaims.db");
+            }
+
+            domain.GetAims();
         }
     }
 }
