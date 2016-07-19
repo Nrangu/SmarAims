6e444b404a89a2e631203ff7d46c5b04f09bb60c
Nrangu <formywork2009@yandex.ru>
2016-07-19 20:50:19 +0300

    19.07.16

--- smartAimsDomain/SmartAimsDomain.cs
+++ smartAimsDomain/SmartAimsDomain.cs
@@ -13,6 +13,7 @@ namespace smartAimsDomain
     public class SQLiteDomain
     {
         private SQLiteConnection connection = null;
+        private SQLiteCommand command = null;
         private string DBname = null;
 
         public bool isConnected {
@@ -27,31 +28,200 @@ namespace smartAimsDomain
             connection.Open();
             return true;
         }
+
+        public string[] GetFields()
+        {
+            
+            command = new SQLiteCommand("SELECT * FROM 'aims';", connection);
+
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            string[] fields = new string[reader.FieldCount];
+
+            for (int i = 0; i < reader.FieldCount ;  i++)
+            {
+                fields[i] = reader.GetName(i);
+            }
+            return fields; 
+
+        }
+
         public void GetAims()
         {
-            //Console.WriteLine("Smart aims Domains");
-            //string databaseName = @"DBaims.db";
-            //SQLiteConnection connection = new SQLiteConnection(string.Format("Data Source={0};", databaseName));
-            //connection.Open();
-            SQLiteCommand command = new SQLiteCommand("SELECT * FROM 'aims';", connection);
+            string[] str;
+            command = new SQLiteCommand("SELECT Count(*) FROM 'aims';", connection);
+
+            object reader1 = command.ExecuteScalar();
+            int count = Convert.ToInt32(reader1.ToString());
+
+
+            Console.WriteLine("count = {0}", count);
+
+            command = new SQLiteCommand("SELECT * FROM 'aims';", connection);
+            
+
+
             SQLiteDataReader reader = command.ExecuteReader();
+
+            string[] fields = new string[ reader.FieldCount];
+
+            
+            for ( int i = 0;  i < reader.FieldCount; i++)
+            {
+                Console.WriteLine(reader.GetName(i));
+                fields[i] = reader.GetName(i);
+            }
+
+            object[,] records = new object[count, reader.FieldCount];
+
+             int k = 0;
             foreach (DbDataRecord record in reader)
             {
-                Console.WriteLine(string.Format("{0} {1} {2} ", record["id"], record["title"], record["description"]));
+                Console.WriteLine(string.Format("{0} {1} {2} ", record["id"].GetType(), record["title"], record["description"]));
+                for (int j = 0; j < reader.FieldCount; j++)
+                {
+                    records[k, j] = record[j];
+                }
+                k++;
             }
         }
+        public DataTable GetData()
+        {
+          
+            command = new SQLiteCommand("SELECT Count(*) FROM 'aims';", connection);
+
+            object reader1 = command.ExecuteScalar();
+            int count = Convert.ToInt32(reader1.ToString());
+
+
+            Console.WriteLine("count = {0}", count);
+
+            command = new SQLiteCommand("SELECT * FROM 'aims';", connection);
+
+
+
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            string[] fields = new string[reader.FieldCount];
+
+
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                Console.WriteLine(reader.GetName(i));
+                fields[i] = reader.GetName(i);
+            }
+
+            object[,] records = new object[count, reader.FieldCount];
+
+            int k = 0;
+            foreach (DbDataRecord record in reader)
+            {
+                Console.WriteLine(string.Format("{0} {1} {2} ", record["id"].GetType(), record["title"], record["description"]));
+                for (int j = 0; j < reader.FieldCount; j++)
+                {
+                    records[k, j] = record[j];
+                }
+                k++;
+            }
+
+            return new DataTable(fields, records);
+        }
     }
+
+    public class DataTable
+    {
+        private string[] _fields;
+        private object[,] _data;
+
+        protected DataTable() { }
+
+        public DataTable(string[] fields, object[,] data)
+        {
+            _fields = fields;
+            _data = data;
+        }
+
+        public bool this[string index] {
+            get {
+
+                for (int i = 0; i < _fields.Length; i++)
+                {
+                    if (index == _fields[i])
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                return _fields[index];
+            }
+        }
+
+        public object this[int index , string field]{
+            get{ /// TODO сделать проверки на ошибки!!!
+                return _data[index, fieldNumber(field)];
+            }
+        }
+
+        protected int fieldNumber(string field)
+        {
+            for( int i = 0; i < _fields.Length; i++  )
+            {
+                if ( field == _fields[i] ) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+
     public class Controller
     {
         private SQLiteDomain domain = new SQLiteDomain();
+
+        private string DBName;
+
+        public Controller()
+        {
+            DBName = @"DBaims.db";
+        }
+
+        public string[] GetFileds()
+        {
+            if (!domain.isConnected)
+            {
+                domain.connect(DBName);
+            }
+            return domain.GetFields();
+        }
+
         public  void GetAims()
         {
             if ( !domain.isConnected)
             {
-                domain.connect(@"DBaims.db");
+                domain.connect(DBName);
             }
 
             domain.GetAims();
+        }
+
+        public void GetAims(out DataTable dataTable)
+        {
+            if (!domain.isConnected)
+            {
+                domain.connect(DBName);
+            }
+
+            dataTable = domain.GetData();
+
         }
     }
 }
