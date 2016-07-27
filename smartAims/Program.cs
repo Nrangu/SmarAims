ef37a4fab3bdcd67c978befa76b09fe98103d0f7
Nrangu <formywork2009@yandex.ru>
2016-07-27 22:51:48 +0300

    1

--- smartAims/Program.cs
+++ smartAims/Program.cs
@@ -8,16 +8,27 @@ namespace smartAims
 {
     class Program
     {
+        static object m()
+        {
+            int k = 11;
+            return k;
+        }
         static void Main(string[] args)
         {
 
+            int k = 10;
+            object k2 = k;
+
+            //int k3 =() m();
+
             Console.WriteLine("test start");
 
-            Controller controller = new Controller();
+            Controller controller = new Controller(@"DBaims.db");
 
             DataTable dataTable;
 
-            controller.GetAims(out dataTable);
+            controller.GetAims( out dataTable );
+            //controller.GetAims();
 
             Console.WriteLine("ok");
             Console.ReadKey();
